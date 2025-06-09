30e97e439e347031129727f779281caf4cec95d8
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-09 18:52:10 +0700

    membuat obyek file dan membuka file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -48,5 +48,14 @@ int main(){
     }
     // selesai dalam menulis sekarang tutup filenya
     outfile.close();
+    
+    // membuat obyek input file stream dalam mode membaca.
+    ifstream infile;
+    // membuka file
+    infile.open(NamaFile + ".txt");
+
+    cout << endl
+         << ">= Membuka dan membaca file " << endl;
+    
 
 }
