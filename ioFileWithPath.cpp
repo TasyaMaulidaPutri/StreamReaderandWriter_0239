91e3a0fb2a9d4379da56fdbfba9c124ccbc4e2cd
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-09 18:35:09 +0700

    membuka file dalam mode menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,5 +10,12 @@ int main(){
     cout << "Masukkan nama File : ";
     cin >> NamaFile;
 
+    // Membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + "txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
 }
