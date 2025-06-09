8f7b98f37657d5fbc9e780d3d7fd3eed2d2f5665
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-09 18:38:28 +0700

    menunjuk ke sebuah nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,5 +30,10 @@ int main(){
     }
     // Membuka file dalam mode menulis.
     ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + "txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
 }
