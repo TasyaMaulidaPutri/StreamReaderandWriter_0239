f76e94a266300046192507d145a4ebef94cd0737
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-09 17:24:23 +0700

    membuka file

--- ioFile.cpp
+++ ioFile.cpp
@@ -8,4 +8,8 @@ int main(){
 
     // membuat obyek output file stream dalam mode menulis.
     ofstream outfile;
+    // membuka file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
