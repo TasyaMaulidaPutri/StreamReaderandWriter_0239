9a4f15773b35906d32ef76850699ee6346309a12
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-09 19:03:25 +0700

    menambahkan fungsi main

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -5,3 +5,21 @@
 // untuk obyek array yang akan kita gunakan
 using namespace std;
 
+int main(){
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array < int, 3 > data = {10, 20, 30};
+        // pesan array integer 3 elemen
+        cout << data.at(4) << endl;
+        // memanggil array elemen ke 5
+    }
+    catch (exception &e)
+    {
+        // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
+}
