72ad69edb7050bed760afb606dc41331cbc83e57
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-06-09 19:08:35 +0700

    last commit

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -13,25 +13,7 @@ int main(){
     // Membuka file dalam mode menulis.
     ofstream outfile;
     // menunjuk ke sebuah nama file
-    outfile.open(NamaFile + "txt", ios::out);
-
-    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
-    // unlimited loop untuk menulis
-    while (true){
-        cout << "-";
-        // mendapatkan setiap karakter dalam satu baris
-        getline(cin, baris);
-        // loop akan berhenti jika anda memasukkan karakter q
-        if (baris == "q")
-            break;
-    // menulis dan memasukkan nilai dari 'baris' ke dalam file
-    outfile << baris << endl;
-    }
-    // Membuka file dalam mode menulis.
-    ofstream outfile;
-    // menunjuk ke sebuah nama file
-    outfile.open(NamaFile + "txt", ios::out);
+    outfile.open(NamaFile + ".txt", ios::out);
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
